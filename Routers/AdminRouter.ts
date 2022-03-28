49714f59de3b1c86296b2d24da3720d61a392848
psycho <59530191+psychologyz@users.noreply.github.com>
2022-03-28 01:28:32 +0000

    Add files via upload

---
+++ Routers/AdminRouter.ts
@@ -0,0 +1,54 @@
+import e, { Request, Response, Router } from 'express';
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
+const path = require("path");
+const router = Router();
+const dotenv = require('dotenv').config()
+const adminKey = process.env.ADMIN_KEY;
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+import makeInvite from '../Utils/makeInvite';
+
+router.post('/makeInvite', multipartMiddleware, async function (req: Request, res: Response) {
+
+
+
+    const sentKey = req.body.key as string;
+    console.log(sentKey);
+
+    if (sentKey !== adminKey) {
+        res.status(401).json({
+            success: false,
+            error: 'Invalid key'
+        });
+        return;
+    }
+
+    const invite = makeInvite();
+
+    const insertInvite = await prisma.invites.create({
+        data: {
+            code: invite
+        }
+    });
+
+    console.log(insertInvite);
+
+    res.status(200).json({
+        success: true,
+        invite
+    });
+
+
+
+
+
+
+
+
+
+});
+
+
+export default router;
